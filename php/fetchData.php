53849a5bb392f96d9fd7de1c7c4d4ecaa9b5472d
Keshav Tangri <tangri57@gmail.com>
2019-10-03 12:50:48 +0530

    Excel Link for Registration Committee

---
+++ php/fetchData.php
@@ -0,0 +1,39 @@
+<?php
+
+	header('Content-Type: text/csv; charset=utf-8');  
+	header('Content-Disposition: attachment; filename=registrations.csv');
+	$conn = mysqli_connect("localhost","apratim","apratim-2238","apratim_2k19");
+
+	if(!$conn){
+		echo "Error Connecting to database !";
+		die();
+	}
+
+	if($stmt = $conn->prepare("SELECT * FROM `registrations` WHERE 1")){
+		if($stmt->execute()){
+			$stmt_result = $stmt->get_result();
+      		$output = fopen("php://output", "w");  
+      		fputcsv($output, array('name/TeamName', 'email', 'contact', 'eventType','eventName','No. of Team Mems','teamLeader','college','teamDdetails'));			
+			while ($row = $stmt_result->fetch_assoc()) {
+				fputcsv($output, $row); 
+			}
+
+			fclose($output);
+			echo "Data Download Successful !";
+			$conn->close();
+			die();
+		}else{
+			echo "Query Execution Failed !";
+			$conn->close();
+			die();
+		}
+	}else{
+		echo "Query Preparation Failed !";
+		$conn->close();
+		die();
+	}
+
+	$conn->close();
+
+
+?>
